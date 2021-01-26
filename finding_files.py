b2ef313e277a3f108765bf733e55727e0f618c55
allison eduardo <mackhyuuga@hotmail.com>
2021-01-26 07:53:30 -0300

    versão não poo

--- finding_files.py
+++ finding_files.py
@@ -1,109 +1,66 @@
-import os
-from os.path import join 
-class Folder:
-    def __init__(self, path = '/'):
-        self.path = path 
-        self.root = []
-        self.files = {}
-        self.file = {}
-        
+import os 
 
+path = '/home/mack/Livros'
+search_for = 'física'
 
-    @property
-    def path(self):
-        return self._path    
+def format_size(size):
+    base = 1024
+    kilo = base 
+    mega = base ** 2
+    giga = base ** 3
+    tera = base ** 4 
+    peta = base ** 5
 
-    @path.setter
-    def path(self, name):
-        self.check(name)
-        self._path = name
+    if size < kilo:
+        text = 'B'
 
-    @staticmethod
-    def check(name):
-        if not isinstance(name, str):
-            raise TypeError('it need to be a string')
-        
+    elif size < mega:
+        size /= kilo
+        text = 'B'
 
-    @staticmethod
-    def format_size(size):
-        base = 1024
-        kilo = base 
-        mega = base ** 2
-        giga = base ** 3
-        tera = base ** 4 
-        peta = base ** 5
+    elif size < giga:
+        size /= mega
+        text = 'M'
 
-        if size < kilo:
-            text = 'B'
+    elif size < tera:
+        size /= giga
+        text = 'G'
+    
+    elif size < peta:
+        size /= tera
+        text = 'T'
 
-        elif size < mega:
-            size /= kilo
-            text = 'B'
+    else:
+        size /= peta 
+        text = 'P'
 
-        elif size < giga:
-            size /= mega
-            text = 'M'
+    size = round(size, 2)
+    return f'{size}{text}'
 
-        elif size < tera:
-            size /= giga
-            text = 'G'
-        
-        elif size < peta:
-            size /= tera
-            text = 'T'
-
-        else:
-            size /= peta 
-            text = 'P'
-
-        size = round(size, 2)
-        return f'{size}{text}'
-            
-            
-
-    def search(self):       
-        for root, directories, files in os.walk(self.path):
-            for file in files:
+counter = 0
+for root, directories, files in os.walk(path):
+    for file in files:
+        if search_for in file:
+            try:
+                counter += 1
                 full_path = os.path.join(root, file)
                 file_name, ext_file = os.path.splitext(file)
                 size = os.path.getsize(full_path)
-                formated_size = self.format_size(size) 
-                self.files.update({file_name: [full_path, ext_file, size, formated_size]})
+                print('file found:', file)
+                print('file path:', path)
+                print('name:', file_name)
+                print('extension:', ext_file)
+                print('size:', size)
+                print('formatted size:', format_size(size))
+                print(end='\n\n')
+            except PermissionError as e:
+                print('without permission')
+            except  FileNotFoundError as e:
+                print('File not found')
+            except Exception as e:
+                print('unknown error')
 
-
-    def find(self, search_for):
-        self.check(search_for)
-        counter = 0
-        for key, value in self.files.items():
-            if search_for in key:
-                self.file.update({key: value})
-                counter +=1
-        return f'{counter} were found'
-
-
-    def show(self):
-        for key, value in self.file.items():
-            print('file found:', key + value[1])
-            print('file path:', value[0])
-            print('name:', key)
-            print('extension:', value[1])
-            print('size:', value[2])
-            print('formatted size:', value[3])
-            print(end='\n\n')
-
-        
-
-
-f1 = Folder('/home/mack/Livros')
-f1.search()
-print(f1.find('física'))
-f1.show()
-
-
-
-
-
-
+print(f'{counter} files were found')
 
 
 
